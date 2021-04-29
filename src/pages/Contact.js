8a0142c0c24ff74b094da79627a91b17ad0ac315
Alex Hernandez <alexandre_2420@outlook.com>
2021-04-29 11:58:04 -0500

    pagina contacto

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,22 @@
+//STYLES
+import styles from '../styles/Contact.module.css'
+
 export const Contact = () => {
     return(
-        <h1>Contact</h1>
+        <article className={ styles.Contact }>
+            <form className={ styles.Form }>
+                <label htmlFor="name">Nombre</label>
+                <input type="text" id='name' placeholder='Ingrese nombre completo'/>
+
+                <label htmlFor="email">Correo Electronico</label>
+                <input type="email" id='email' placeholder='ejemplo@email.com'/>
+
+                <label htmlFor="message">Message</label>
+                <textarea name="message" id="message" cols="30" rows="10" placeholder='Mensage'></textarea>
+
+                <input type="submit" value='Enviar'/>
+
+            </form>
+        </article>
     )
 }
